5e141e78499518e137c656058f09c05bb021edfb
Benjamin Gondange <benjamin.gondange@reseau.eseo.fr>
2020-11-03 17:12:12 +0100

    Remove details

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from 'next/head'
-import LoginForm from '../components/LoginForm'
 
 export default function Home() {
   return (
     <div className="container mx-auto px-4">
       <Head>
-        <title>Mon Informaticien</title>
+        <title>Hello world</title>
       </Head>
 
       <main className="">
